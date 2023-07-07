3dbbc15d40ed793c494171fe4247f421e37c2847
Lobo-rio <gsilvamedeiros@gmail.com>
2023-07-07 10:13:11 -0300

    feat: test unit complete orgs create

---
+++ src/repositories/in-memory/in-memory-orgs-repository.ts
@@ -0,0 +1,44 @@
+import { Org, Prisma } from "@prisma/client";
+import { OrgsRepository } from "../abstract/orgs-repository";
+
+export class InMemoryOrgsRepository implements OrgsRepository {
+    public items: Org[] = []
+
+    async findByEmail(email: string) {
+        const org = this.items.find(item => email === item.email)
+
+        if (!org) return null
+
+        return org
+    }
+    async findByCelular(celular: string) {
+        const org = this.items.find(item => celular === item.celular)
+
+        if (!org) return null
+
+        return org
+    }
+
+    async findMany() {
+        return this.items
+    }
+    
+    async create(data: Prisma.OrgCreateInput) {
+        const org = { 
+            id: 'test-1',
+            title: data.title,
+            celular: data.celular,
+            address: data.address,
+            city: data.city, 
+            email: data.email, 
+            password: data.password,
+            createdAt: new Date(),
+            updatedAt: new Date(),
+        }
+
+        this.items.push(org)
+
+        return org
+    }
+
+}
--- src/use-cases/orgs/register-orgs.spec.ts
+++ src/use-cases/orgs/register-orgs.spec.ts
@@ -1,27 +1,28 @@
 import { RegisterOrgsUseCase } from "./register-orgs"
 import { compare } from "bcryptjs"
-import { Org } from "@prisma/client"
+import { InMemoryOrgsRepository } from "@/repositories/in-memory/in-memory-orgs-repository"
+import { OrgsAlreadyExistsError } from "../errors/orgs-already-exists-error"
 
 describe('Register Orgs Use Case', () => {
+    it('should be able to register org', async () => {
+        const orgsRepository = new InMemoryOrgsRepository()
+        const registerOrgsUseCase = new RegisterOrgsUseCase(orgsRepository) 
+
+        const { org } = await registerOrgsUseCase.execute({
+            title: 'Fala Mansa',
+            celular: '99999999999',
+            address: 'Rua Test Vitest',
+            city: 'Vistest', 
+            email: 'string@test.com', 
+            password: 'string908',
+        })
+
+        expect(org.id).toEqual(expect.any(String))
+    })
+
     it('should hash orgs password upon registration', async () => {
-        const registerOrgsUseCase = new RegisterOrgsUseCase({
-            async findByEmail(email: string) { return null},
-            async findByCelular(celular: string) { return null},
-            async findMany() { return [] },
-            async create(data) {
-                return { 
-                    id: 'test-1',
-                    title: data.title,
-                    celular: data.celular,
-                    address: data.address,
-                    city: data.city, 
-                    email: data.email, 
-                    password: data.password,
-                    createdAt: new Date(),
-                    updatedAt: new Date(),
-                }
-            }
-        }) 
+        const orgsRepository = new InMemoryOrgsRepository()
+        const registerOrgsUseCase = new RegisterOrgsUseCase(orgsRepository) 
 
         const { org } = await registerOrgsUseCase.execute({
             title: 'Fala Mansa',
@@ -38,5 +39,65 @@ describe('Register Orgs Use Case', () => {
         )
 
         expect(isPasswordCorrectlyHashed).toBe(true)
+    })
+
+    it('should not be able to register with same email twice', async () => {
+        const orgsRepository = new InMemoryOrgsRepository()
+        const registerOrgsUseCase = new RegisterOrgsUseCase(orgsRepository) 
+
+        const email = 'string@test.com'
+
+        await registerOrgsUseCase.execute({
+            title: 'Fala Mansa',
+            celular: '99999999999',
+            address: 'Rua Test Vitest',
+            city: 'Vistest', 
+            email, 
+            password: 'string908',
+        })
+
+        
+        expect(
+            async () => {
+                await registerOrgsUseCase.execute({
+                    title: 'Fala Mansa',
+                    celular: '99999999999',
+                    address: 'Rua Test Vitest',
+                    city: 'Vistest', 
+                    email, 
+                    password: 'string908',
+                })
+            }
+        ).rejects.toBeInstanceOf(OrgsAlreadyExistsError)
+    })
+
+    it('should not be able to register with same celular twice', async () => {
+        const orgsRepository = new InMemoryOrgsRepository()
+        const registerOrgsUseCase = new RegisterOrgsUseCase(orgsRepository) 
+
+        const celular = '21999999999'
+
+        await registerOrgsUseCase.execute({
+            title: 'Fala Mansa',
+            celular,
+            address: 'Rua Test Vitest',
+            city: 'Vistest', 
+            email: 'string@test.com', 
+            password: 'string908',
+        })
+
+        
+        expect(
+            async () => {
+                await registerOrgsUseCase.execute({
+                    title: 'Fala Mansa',
+                    celular,
+                    address: 'Rua Test Vitest',
+                    city: 'Vistest', 
+                    email: 'string@test.com', 
+                    password: 'string908',
+                })
+            }
+        ).rejects.toBeInstanceOf(OrgsAlreadyExistsError)
     })
 })
